a1bf2f9fd3244d7bbda3351ac3d95c1faee186c3
Samuel Souza <silva.samueldev@gmail.com>
2026-01-09 18:38:33 -0300

    :recycle: refactor: changing <a> to <Link>

--- src/app/components/gohome/GoHome.tsx
+++ src/app/components/gohome/GoHome.tsx
@@ -1,11 +1,12 @@
+import { Link } from "react-router-dom"
 
 const GoHome = () => {
     return (
-        <a href="/iron-key/" className="bg-blue-500 text-white flex items-center gap-2 px-2 py-1 rounded shadow-md font-bold hover:scale-105 hover:bg-blue-400 transition-all">
+        <Link to="/" className="bg-blue-500 text-white flex items-center gap-2 px-2 py-1 rounded shadow-md font-bold hover:scale-105 hover:bg-blue-400 transition-all">
             <img src="/iron-key/assets/icons/house.png"
                 alt="Home Icon"
                 className="h-4" />Go Home
-        </a>
+        </Link>
     )
 }
 
--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 const Header = () => {
     return (
         <header className="flex justify-around items-center pt-2 border-b pb-2 border-gray-300">
@@ -6,7 +8,7 @@ const Header = () => {
                     <img src="/iron-key/assets/logo/key-logo.png" alt="Iron Key Logo" className="h-6" />
                     <p className="text-gray-500 font-bold">Iron Key</p>
                 </div>
-                <a className="text-gray-500" href="/">Home</a>
+                <Link className="text-gray-500" to="/">Home</Link>
             </aside>
             <a href="https://github.com/devsamuca/iron-key"><img src="/iron-key/assets/icons/github.png" alt="Iron Key Logo" className="h-9 hover:bg-gray-50 p-1 rounded" /></a>
         </header>
